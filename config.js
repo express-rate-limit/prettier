b6fd5a24fc9bd1288736e96575fc14c29c70b7f9
Vedant K <gamemaker0042@gmail.com>
2023-08-18 18:37:40 +0530

    feat: initial commit

---
+++ config.js
@@ -0,0 +1,14 @@
+// config.js
+// The prettier configuration for express-rate-limit projects.
+
+const config = {
+	semi: false,
+	useTabs: true,
+	singleQuote: true,
+	bracketSpacing: true,
+	trailingComma: "all",
+	proseWrap: "always",
+	endOfLine: "lf",
+};
+
+module.exports = config;
